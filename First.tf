13e6bd4d00c612e0c12b4c453246c50e6e2c2a3a
ganeshvarmav <ssganeshvarma@gmail.com>
2024-12-10 19:28:02 +0530

    First tf file

---
+++ First.tf
@@ -0,0 +1,15 @@
+# Specify the provider (Azure in this case)
+provider "azurerm" {
+  features {} # Required for the Azure provider
+}
+
+# Create a resource group in Azure
+resource "azurerm_resource_group" "hello_world_rg" {
+  name     = "HelloWorldResourceGroup"
+  location = "East US"
+}
+
+# Output the resource group name
+output "resource_group_name" {
+  value = azurerm_resource_group.hello_world_rg.name
+}
